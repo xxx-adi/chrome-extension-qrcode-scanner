eff67951051499db00c7d2532cb790667add90bc
Adi <adi.zheng@hotmail.com>
2023-06-25 00:57:59 +0800

    scan qr code and log result

--- src/content.js
+++ src/content.js
@@ -1,46 +1,13 @@
-// import { BrowserQRCodeReader } from '@zxing/browser';
+import QrScanner from 'qr-scanner';
 
-console.log('content.js')
 chrome.runtime.onMessage.addListener(function (request) {
   const { action, payload } = request;
   console.log('content onMessage', request);
   if (action === 'captureVisibleTab' && payload) {
-    var link = document.createElement('a');
-    console.log(payload);
-    link.href = payload;
-    link.download = 'screenshot.png';
-    // link.click();
-
-    // const codeReader = new BrowserQRCodeReader()
-    // console.log('ZXing code reader initialized')
-
-    // codeReader.decodeFromImage(payload).then((result) => {
-    //   console.log(result)
-    // }).catch((err) => {
-    //   console.error(err)
-    // })
-
-    // // 传递图像数据进行二维码解析
-    // ZXing.decode(dataUrl)
-    //   .then(function (result) {
-    //     // 解析结果
-    //     console.log('二维码内容:', result.text);
-    //   })
-    //   .catch(function (error) {
-    //     // 解析失败
-    //     console.error('解析二维码失败:', error);
-    //   });
-
-    // // 获取图像中的二维码位置和边界框信息
-    // ZXing.scan(dataUrl)
-    //   .then(function (result) {
-    //     // 二维码在图像上的位置和边界框
-    //     console.log('二维码位置:', result.location);
-    //     console.log('二维码边界框:', result.boundingBox);
-    //   })
-    //   .catch(function (error) {
-    //     // 扫描失败
-    //     console.error('扫描二维码失败:', error);
-    //   });
+    const img = new Image();
+    img.src = payload;
+    QrScanner.scanImage(img, { returnDetailedScanResult: true })
+      .then(result => console.log(result))
+      .catch(error => console.log(error || 'No QR code found.'));
   }
 });
